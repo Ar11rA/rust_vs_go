9a9d4d44b8e040b3de1e00589cb4831bcdeafd3c
Aritra <aritra119.26@gmail.com>
2020-08-19 12:05:12 +0530

    Add missing problem link

--- rust/src/problems/find_and_replace_pattern.rs
+++ rust/src/problems/find_and_replace_pattern.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::iter::FromIterator;
 
+//https://leetcode.com/problems/find-and-replace-pattern/
 pub fn find_and_replace_pattern(words: Vec<String>, pattern: String) -> Vec<String> {
     return words
         .into_iter()
